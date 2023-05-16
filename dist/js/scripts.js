765691a158408b8a673bedd704527334756c093e
Batul Chehab <ec21455@qmul.ac.uk>
2023-05-16 23:21:55 +0100

    first commit

---
+++ dist/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Comrat One-Page v6.0.6 (undefined)
+* Copyright 2013-2023 undefined
+* Licensed under undefined (https://github.com/StartBootstrap/comrat-one-page/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
